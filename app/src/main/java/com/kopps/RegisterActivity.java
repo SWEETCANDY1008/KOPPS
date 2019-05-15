1082ca0507b2e11d1ec923d6d050c46b7f0c00b5
sweetcandy1008 <anjwld1008@gmail.com>
2019-05-15 22:02:25 +0900

    miner upload
    2019.05.15 miner upload

--- app/src/main/java/com/kopps/RegisterActivity.java
+++ app/src/main/java/com/kopps/RegisterActivity.java
@@ -1,11 +1,18 @@
 package com.kopps;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
+import android.text.style.BackgroundColorSpan;
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
@@ -83,22 +90,77 @@ public class RegisterActivity extends AppCompatActivity implements BeaconConsume
     private void logToDisplay() {
         final LinearLayout linearLayout = (LinearLayout) RegisterActivity.this.findViewById(R.id.linearlayout);
 
+
         runOnUiThread(new Runnable() {
             public void run() {
-                TextView[] textviews = new TextView[beaconList.size()];
+//                TextView[] textviews = new TextView[beaconList.size()];
 
                 for (Beacon beacon : beaconList) {
                     int test = beaconList.indexOf(beacon);
-//
-                    textviews[test] = new TextView(RegisterActivity.this);
-                    textviews[test].setTag("beacon"+beacon.getId2());
 
+                    // 비콘들의 정보가 들어가는 레이아웃
+                    LinearLayout beaconlayout = new LinearLayout(RegisterActivity.this);
+
+                    // 비콘의 이름이나 기타 텍스트를 작성하기 위한 뷰
+                    TextView textviews = new TextView(RegisterActivity.this);
+
+                    // 비콘의 태그를 정해주기 위함
+                    textviews.setTag("beacon"+beacon.getId2());
+
+                    Button button = new Button(RegisterActivity.this);
+                    button.setTag("beaconAddButton"+beacon.getId2());
+                    button.setText("추가");
+//                    button.setGravity(Gravity.RIGHT|Gravity.CENTER_VERTICAL);
+
+                    LinearLayout.LayoutParams pm2 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT); //레이아웃파라미터 생성
+                    pm2.setMargins(0, 10, 10, 10);
+                    pm2.gravity = Gravity.CENTER_VERTICAL | Gravity.RIGHT;
+                    button.setLayoutParams(pm2);
+
+
+                    final int height = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 100, getResources().getDisplayMetrics());
+
+                    LinearLayout.LayoutParams pm = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT); //레이아웃파라미터 생성
+                    pm.weight = 3;
+                    pm.width = LinearLayout.LayoutParams.WRAP_CONTENT; //버튼의 너비를 설정(픽셀단위로도 지정가능)
+                    pm.height = height; //버튼의 높이를 설정(픽셀단위로도 지정 가능)
+                    pm.setMargins(10, 10, 0, 10);
+                    pm.gravity = Gravity.CENTER_VERTICAL;
+                    textviews.setLayoutParams(pm);
+
+                    // 각 비콘별로 레이아웃을 생성할 때 기존에 존재하는지 확인하기 위함
                     if(linearLayout.findViewWithTag("beacon"+beacon.getId2()) == null || linearLayout.findViewWithTag("beacon"+beacon.getId2()).equals(null)) {
-                        linearLayout.addView(textviews[beaconList.indexOf(beacon)]);
+                        beaconlayout.setBackgroundColor(Color.rgb(255,255,255));
+                        beaconlayout.addView(textviews);    // 각 비콘별 레이아웃에 텍스트뷰를 추가
+                        // 버튼 추가란
+                        beaconlayout.addView(button);
+                        linearLayout.addView(beaconlayout);
+
                     } else {
                         TextView textView = linearLayout.findViewWithTag("beacon"+beacon.getId2());
                         textView.setText("major : " + beacon.getId2() + " Distance : " + String.format("%.3f", beacon.getDistance()) + " meters." + beacon.getRssi() + "\n");
+                        textView.setGravity(Gravity.CENTER_VERTICAL);
                     }
+
+//                    textviews[test] = new TextView(RegisterActivity.this);
+//                    textviews[test].setTag("beacon"+beacon.getId2());
+//
+//                    if(linearLayout.findViewWithTag("beacon"+beacon.getId2()) == null || linearLayout.findViewWithTag("beacon"+beacon.getId2()).equals(null)) {
+//                        linearLayout.addView(textviews[beaconList.indexOf(beacon)]);
+//                    } else {
+//                        TextView textView = linearLayout.findViewWithTag("beacon"+beacon.getId2());
+//                        textView.setText("major : " + beacon.getId2() + " Distance : " + String.format("%.3f", beacon.getDistance()) + " meters." + beacon.getRssi() + "\n");
+//                    }
+                    final Beacon beacon_test = beacon;
+
+                    Button buttons = (Button) button.findViewWithTag("beaconAddButton" + beacon.getId2());
+                    buttons.setOnClickListener(new Button.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Toast.makeText(getApplicationContext(), "눌렸습니다. 비콘의 이름은" + beacon_test.getId2() + "입니다.", Toast.LENGTH_LONG).show();
+                        }
+                    });
+
                     Log.d(TAG, "major : " + beacon.getId2() + " Distance : " + String.format("%.3f", beacon.getDistance())+ " meters away." + beacon.getRssi() + "\n");
                     Log.d(TAG, beacon.toString());
                 }
